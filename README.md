a805ebd7c6036fa4387c25a150821025818d64f4
周群华 <qunhua.zhou@audaque.com>
2019-12-19 16:43:48 +0800

    text-to-svg

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # text-to-svg
-text-to-svg
+
+## Project setup
+```
+npm install
+```
+
+### Compiles
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,13 @@
+const TextToSVG = require('text-to-svg');
+const fs = require('fs');
+
+const textToSVG = TextToSVG.loadSync('./fonts/FrederickatheGreat-Regular.ttf');
+
+const attributes = {stroke: 'black'};
+const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
+const text = 'STH'
+const svg = textToSVG.getSVG(text, options);
+
+fs.writeFileSync(`./svgs/${text}.svg`,`<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">${svg}` )
+console.log(`./svg/${text}.svg created.`)
