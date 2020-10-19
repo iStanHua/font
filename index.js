a886e8b46064c0f5ce0a3fb9b9c5e19b82731f8c
周群华 <qunhua.zhou@audaque.com>
2020-10-19 11:29:26 +0800

    ttf

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-const TextToSVG = require('text-to-svg');
-const fs = require('fs');
+const TextToSVG = require('text-to-svg')
+const fs = require('fs')
 
-const textToSVG = TextToSVG.loadSync('./fonts/handlee-v8-latin-regular.ttf');
+class App {
+  constructor() {
 
-const attributes = {stroke: 'black'};
-const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
-const text = 'sTy'
-const svg = textToSVG.getSVG(text, options);
+  }
 
-fs.writeFileSync(`./svgs/${text}.svg`,`<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">${svg}` )
-console.log(`./svg/${text}.svg created.`)
+  init() {
+
+    const ttfList = [
+      'ALLED',
+      'FB3222CCA9341F69',
+      'FrederickatheGreat-Regular',
+      'Geostar-Regular',
+      'handlee-v8-latin-regular',
+      'LESLIEB'
+    ]
+
+    ttfList.forEach(t => {
+      const textToSVG = TextToSVG.loadSync(`./fonts/${t}.ttf`)
+      const text = 'H'
+      const svg = textToSVG.getSVG(text, {
+        x: 0,
+        y: 0,
+        fontSize: 72,
+        anchor: 'top',
+        attributes: {
+          stroke: 'black'
+        }
+      })
+
+      fs.writeFileSync(`./svgs/${text}_${t}.svg`, `<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">${svg}`)
+
+      console.log(`./svg/${text}_${t}.svg created.`)
+
+    })
+  }
+}
+
+new App().init()
